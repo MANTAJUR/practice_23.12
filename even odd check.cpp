ceee534f7fc5fc6145313f39b2da48682d24d5ed
Sk Mantajur Islam <120780547+MANTAJUR@users.noreply.github.com>
2022-12-23 19:30:08 +0530

    Add files via upload

---
+++ even odd check.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int number=0;
+	printf("enter number:\n");
+	scanf("%d", &number);
+	 
+	if(number>=0){printf("positive\n");
+	if(number%2==0){printf("even number\n");}  
+	 else{printf("odd number\n");
+	}
+	
+}
+	printf("thanks");
+	return 0;
+	}
